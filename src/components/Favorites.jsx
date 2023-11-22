174bb2e24cc2a42d876efd3460a3ce8ce47210a3
Bhairav <bhairavgotam066@gmail.com>
2023-11-22 14:43:32 +0530

    added betalectic-assignment

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -1,65 +1,91 @@
 // Favorites.js
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Favorites = ({ favorites, removeFromFavorites, addToFavorites }) => {
+  const navigate = useNavigate();
   const [showModal, setShowModal] = useState(false);
+  const [selectedPackage, setSelectedPackage] = useState(null);
 
-  const handleAddToFavorites = (packageName, reason) => {
-    // Add validation logic here
-    addToFavorites({ packageName, reason });
+  const handleViewPackage = (packageName) => {
     setShowModal(true);
-
-    // Save favorites to local storage
-    localStorage.setItem('favorites', JSON.stringify([...favorites, { packageName, reason }]));
+    setSelectedPackage(packageName);
   };
 
-  const handleCloseModal = () => {
-    setShowModal(false);
+  const handleEditPackage = (packageName) => {
+    console.log('Editing package:', packageName);
+  };
+
+  const handleDeletePackage = (packageName) => {
+    removeFromFavorites(packageName);
   };
 
   useEffect(() => {
-    // Retrieve favorites from local storage on mount
     const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
     addToFavorites(storedFavorites);
   }, [addToFavorites]);
 
   return (
-    <div>
-      <h2 className="text-2xl font-bold mb-4">Welcome to the favorite NPM packages</h2>
+    <div className="container mx-auto my-8 p-4 bg-gray-100">
+      <h2 className="text-2xl font-bold mb-4 mt-10">Welcome to the favorite NPM packages</h2>
 
       {favorites.length === 0 ? (
-        <div className="text-center">
+        <div className="mb-8">
           <p>You don't have any favs yet, please add.</p>
           <button
             className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-            onClick={() => setShowModal(true)}
+            onClick={() => navigate('/favorites')}
           >
             Add Fav
           </button>
         </div>
       ) : (
-        <ul>
-          {favorites.map((favorite) => (
-            <li key={favorite.packageName} className="mb-2 p-2 border border-gray-300 rounded">
-              <span className="mr-2">{favorite.packageName}</span> - {favorite.reason}
-              <button
-                className="ml-2 p-2 bg-red-500 text-white rounded hover:bg-red-700"
-                onClick={() => removeFromFavorites(favorite.packageName)}
-              >
-                Remove
-              </button>
-            </li>
-          ))}
-        </ul>
+        <div className="mt-4">
+          <table className="min-w-full bg-white border border-gray-300">
+            <thead>
+              <tr className="bg-gray-200">
+                <th className="py-2 px-4">Package Name</th>
+                <th className="py-2 px-4">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {favorites.map((favorite) => (
+                <tr key={favorite.packageName} className="border-b border-gray-300">
+                  <td className="py-2 px-4">{favorite.packageName}</td>
+                  <td className="py-2 px-4">
+                    <button
+                      className="mr-2 p-1 bg-green-500 text-white rounded hover:bg-green-700"
+                      onClick={() => handleViewPackage(favorite.packageName)}
+                    >
+                      View
+                    </button>
+                    <button
+                      className="mr-2 p-1 bg-blue-500 text-white rounded hover:bg-blue-700"
+                      onClick={() => handleEditPackage(favorite.packageName)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className="p-1 bg-red-500 text-white rounded hover:bg-red-700"
+                      onClick={() => handleDeletePackage(favorite.packageName)}
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
 
       {showModal && (
-        <div className="fixed inset-0 flex items-center justify-center">
-          <div className="bg-gray-800 bg-opacity-75 p-4 rounded">
-            <p className="text-white">Great! You have added a favorite NPM package.</p>
+        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 p-4 rounded">
+          <div className="bg-white p-4 rounded">
+            <p className="text-gray-800">Viewing package: {selectedPackage}</p>
             <button
               className="mt-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-700"
-              onClick={handleCloseModal}
+              onClick={() => setShowModal(false)}
             >
               Close
             </button>
